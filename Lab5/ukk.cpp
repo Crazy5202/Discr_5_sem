8b10e3a6428d9b07fbbb10f94dc20b5122f167a5
maxim <mdkutsenko@mai.education>
2024-09-24 22:24:13 +0300

    Continuing work

--- Lab5/ukk.cpp
+++ Lab5/ukk.cpp
@@ -2,64 +2,81 @@
 #include <fstream>
 #include <vector>
 #include <chrono>
+#include <map>
 
-class Edge
+class Node
 {
 public:
-    Edge(std::string _path, int _leaf);
+    Node(Node* _parent, int _start_ind, int* _end_ind);
 
-    std::string path;
-    int leaf;
-    std::vector<Edge*> children;
+    int get_length() {return (*end_ind - start_ind + 1);}
+
+    Node* parent;
+    Node* suffix_link;
+    int start_ind;
+    int* end_ind;
+    std::map<int, Node*> children;
 };
 
-Edge::Edge(std::string _path, int _leaf) : path(_path), leaf(_leaf) {}
+Node::Node(Node* _parent, int _start_ind, int* _end_ind) : parent(_parent), start_ind(_start_ind), end_ind(_end_ind) {
+    suffix_link = nullptr;
+}
 
 class STree
 {
 public:
-    STree(std::string text);
+    STree(std::string _text);
 private:
-    void insert(Edge* edge, std::string suf);
-    Edge* make_Edge(std::string _path, int _leaf);
-    Edge* make_empty_Edge();
+    void SPA(int i); // алгоритм одной фазы
+    int SEA(int i, int j); // алгоритм одного продолжения фазы
+    void split(); // правило 2 - разбиение дуги
+    Node* skip_count(Node* start_node, int start, int end); // приём 1 - пропуск дуг в зависимости от длин
 
-    void insert();
+    void add_child(Node* parent, Node* child);
     
-    std::vector<Edge*> edges;
+    Node* root;
+    bool made_node;
+    int* end_ind; // глобальный индекс конца суффикса i для листов
+    std::string text;
+    Node* prev_ext; // указатель на текущую полную строку S[1..i]
+    int j_i; // индекс последнего продолжения, в котором не применялось правило 3 - окончание в дуге
 };
 
-STree::STree(std::string text) {
-    text+="$";
-    edges.push_back(make_Edge(text, 1));
-    for (size_t i=0; i<text.size(); ++i) {
-        std::string word = text.substr(i);
-        bool inserted = false;
-        for (size_t j=0; j<edges.size(); ++j) {
-            if (edges[j]->path[0]==word[0]) {
-                insert(edges[j], word);
-                inserted = true;
-                break;
-            }
-        }
-        if (!inserted) edges.push_back(make_Edge(word, i+1));
+STree::STree(std::string _text) : text(_text+'$') {
+    made_node = false;
+    end_ind = new int(0);
+    root = new Node(nullptr, 0, end_ind);
+    j_i = 1;
+    prev_ext = new Node(root, 0, end_ind);
+    add_child(root, prev_ext);
+    for (int i=1; i<text.size(); ++i) {
+        SPA(i);
     }
 }
 
-void STree::insert(Edge* edge, std::string suf) {
-    
+void STree::SPA(int i) {
+    (*end_ind)++;
+    for (int j = j_i+1; j<= i+1; ++j) {
+        if (SEA(i, j) == 3) break;
+    }
 }
 
-STree::~STree() {}
-
-Edge* STree::make_Edge(std::string _path, int _leaf) {
-    Edge* new_Edge = new Edge(_path, _leaf);
-    return new_Edge;
+int STree::SEA(int i, int j) {
+    Node* next_node;
+    if (prev_ext->suffix_link != nullptr) { // переходим либо по суффиксной ссылке, либо в корень
+        next_node = prev_ext->suffix_link;
+    } else {
+        next_node = root->children[text[j]];
+    }
+    skip_count(next_node);
 }
 
-Edge* STree::make_empty_Edge() {
-    Edge* new_Edge = new Edge("",0);
-    return new_Edge;
+Node* STree::skip_count(Node* start_node, int start, int end) {
+    if (Node->)
+}
+
+void STree::add_child(Node* parent, Node* child) {
+    parent->children[text[child->start_ind]] = child;
 }
 
 int main() {
